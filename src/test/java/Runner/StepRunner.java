4c7bfb52baed32eca8d4feae7322a7734ebe646d
JorgeLazoM <u20219496@utp.edu.pe>
2024-07-25 13:19:46 -0500

    first commit

---
+++ src/test/java/Runner/StepRunner.java
@@ -0,0 +1,56 @@
+package Runner;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.junit.UsePlaywright;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+
+import Step.StepOrangeHRM;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+@UsePlaywright
+public class StepRunner {
+    static Playwright playwright;
+    static Browser browser;
+    BrowserContext context;
+    static Page page;
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
+
+    @Test
+    void test1(Page page) {
+        Page StepOrangeHRM = browser.newPage();
+        StepOrangeHRM searchPage = new StepOrangeHRM(page);
+        searchPage.navigate();
+        searchPage.login("Admin","admin123");
+        searchPage.addUsers("Juan","Luis","Garcia");
+        searchPage.addDetails("333","123456","2025-12-30");
+    }
+
+}
---
+++ src/test/java/Step/StepOrangeHRM.java
@@ -0,0 +1,74 @@
+package Step;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class StepOrangeHRM {
+    private final Page page;
+    private final Locator userName;
+    private final Locator password;
+    private final Locator loginButton;
+    private final Locator addUser;
+    private final Locator firstName;
+    private final Locator middleName;
+    private final Locator lasName;
+    private final Locator regiterSubmit;
+    private final Locator pim;
+
+    private final Locator otherId;
+    private final Locator licenseNum;
+    private final Locator licenseDate;
+    private final Locator checkBoxMale;
+    private final Locator savePersonDetails;
+
+
+    public StepOrangeHRM(Page page) {
+        this.page = page;
+        this.userName = page.getByPlaceholder("Username");
+        this.password = page.getByPlaceholder("Password");
+        this.loginButton = page.locator("#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div.oxd-form-actions.orangehrm-login-action > button");
+        this.addUser = page.locator("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[2]/div[1]/button");
+        this.firstName = page.getByPlaceholder("First Name");
+        this.middleName = page.getByPlaceholder("Middle Name");
+        this.lasName = page.getByPlaceholder("Last Name");
+        this.regiterSubmit = page.locator("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]");
+        this.pim = page.locator("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a");
+        this.otherId = page.locator("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[1]/div[2]/div/div[2]/input");
+        this.licenseNum = page.locator("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[2]/div[1]/div/div[2]/input");
+        this.licenseDate = page.locator("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[2]/div[2]/div/div[2]/div/div/input");
+        this.checkBoxMale = page.locator("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[2]/div[2]/div/div[2]/div[1]/div[2]/div/label/input");
+        this.savePersonDetails = page.locator("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[4]/button");
+    }
+
+    public void navigate() {
+        page.navigate("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    public void login(String user,String pass) {
+        userName.fill(user);
+        password.fill(pass);
+        loginButton.press("Enter");
+        assertThat(page).hasURL("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
+        System.out.println("Paso correctamente el login");
+    }
+
+    public void addUsers(String fname,String mname,String lname){
+        pim.click();
+        addUser.click();
+        assertThat(page).hasURL("https://opensource-demo.orangehrmlive.com/web/index.php/pim/addEmployee");
+        firstName.fill(fname);
+        middleName.fill(mname);
+        lasName.fill(lname);
+        regiterSubmit.click();
+        System.out.println("Paso correctamente agregar usuario");
+    }
+
+    public void addDetails(String otherID,String licensN,String licensD){
+        otherId.fill(otherID);
+        licenseNum.fill(licensN);
+        licenseDate.fill(licensD);
+        savePersonDetails.click();
+    }
+}
